1f7e5de87425f6aca37ffefa43a5ffb2a2d0494a
M Algah Fattah Illahi <herppratama@gmail.com>
2021-04-05 17:21:43 +0700

    update PlayerCard style

---
+++ src/components/PlayerCard/index.tsx
@@ -0,0 +1,53 @@
+import { Avatar, Grid } from '@material-ui/core';
+
+import react from 'react';
+import { Player } from '../../types';
+import { useStyles } from './style';
+
+interface Prop {
+  players: Player[];
+}
+
+const PlayerCard = (properties: Prop) => {
+  const { players } = properties;
+  const styles = useStyles();
+  console.log(`total player: ${players.length}`)
+
+  const degree = Math.PI / 180;
+  const fraction = 360 / players.length;
+
+  const renderPlayer = players.map((item: Player, index: number) => {
+    return (
+      <Grid
+        key={`player-${index}`}
+        style={{
+          transform: `translate(${Math.cos(fraction * index * degree) * 120}px,
+          ${Math.sin(fraction * index * degree) * 120}px)`,
+          position: 'absolute'
+        }}
+      >
+        <Grid
+          className={styles.avatarCont}
+        >
+          <Avatar
+            className={styles.avatar}
+            alt={item.name}
+            src={item.avatar_url}
+          >
+
+          </Avatar>
+        </Grid>
+      </Grid>
+    );
+  });
+
+  return (
+    <Grid
+      className={styles.container}
+    >
+      {renderPlayer}
+    </Grid>
+  )
+}
+
+export default PlayerCard;
---
+++ src/components/PlayerCard/style.tsx
@@ -0,0 +1,24 @@
+import { makeStyles } from '@material-ui/core';
+
+export const useStyles = makeStyles(theme => ({
+  avatar: {
+    margin: theme.spacing(1),
+    height: theme.spacing(8),
+    width: theme.spacing(8),
+    // position: 'absolute'
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    position: 'absolute'
+  },
+  confirm: {
+    margin: theme.spacing(3, 0, 2)
+  },
+  avatarCont: {
+    position: 'absolute'
+  },
+  container: {
+    position: 'relative'
+  }
+}));
