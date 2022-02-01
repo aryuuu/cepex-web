ef26c1d83f03fbdb98408a38e0a030345266b148
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2022-02-01 22:31:18 +0700

    clean up code comments

--- src/components/LeaderboardCard/index.tsx
+++ src/components/LeaderboardCard/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
   Grid,
-  GridList,
   Paper,
   Table,
   TableBody,
@@ -29,12 +28,6 @@ const LeaderboardCard = (properties: Prop) => {
         <TableCell align="center">{item.name}</TableCell>
         <TableCell align="center">{item.score}</TableCell>
       </TableRow>
-      // <Grid
-      //   className={index % 2 === 0 ? styles.itemEven : styles.itemOdd}
-      //   key={`item-${index}`}
-      // >
-      //   <b>#{index+1} {item.avatar_url} {item.name} {item.score}</b>
-      // </Grid>
     )
   })
 
@@ -43,13 +36,6 @@ const LeaderboardCard = (properties: Prop) => {
       className={styles.leaderboard}
     >
       <Typography align="center">Leaderboard</Typography>
-      {/* <GridList
-        className={styles.itemList}
-        cols={1}
-        cellHeight='auto'
-      >
-        {renderItem}
-      </GridList> */}
       <TableContainer component={Paper}>
       <Table>
         <TableHead>
@@ -61,20 +47,6 @@ const LeaderboardCard = (properties: Prop) => {
         </TableHead>
           <TableBody>
             {renderItem}
-          {/* {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))} */}
         </TableBody>
       </Table>
     </TableContainer>
